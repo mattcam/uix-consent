8b976ce74f5fe0f0ae11ddac24f02fb8ad7f552b
risto <risto.kowaczewski@gmail.com>
2024-04-28 23:22:12 +0200

    redo of content widget.

--- src/PoweredBy.js
+++ src/PoweredBy.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function PoweredBy() {
     return <span style={{right: 20, position: "absolute", color: "gray"}}>Powered by <a href="http://www.tracardi.com" style={{color: "black"}}>TRACARDI</a></span>
 }
