46e57476724e492e1f6ae939e1722817a3d4d2a9
risto <risto.kowaczewski@gmail.com>
2024-05-07 19:27:48 +0200

    new cookie center

--- src/PoweredBy.js
+++ src/PoweredBy.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function PoweredBy() {
-    return <span style={{color: "gray", whiteSpace: "nowrap"}}>Powered by <a href="http://www.tracardi.com" style={{color: "black"}}>TRACARDI</a></span>
+    return <span style={{fontSize: 14, color: "gray", whiteSpace: "nowrap"}}>Powered by <a href="http://www.tracardi.com" style={{color: "black"}}>TRACARDI</a></span>
 }
