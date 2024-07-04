512b9ec596786e90faa6a1dab7a8d160abe0b7d2
matt cameron <matt@mattcameron.me>
2024-07-04 10:22:54 +1000

    Change to hide powered by Tracardi when viewed on mobile device

--- src/PoweredBy.js
+++ src/PoweredBy.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function PoweredBy() {
-    return <span style={{right: 20, position: "absolute", color: "gray"}}>Powered by <a href="http://www.tracardi.com" style={{color: "black"}}>TRACARDI</a></span>
+    return <span style={{right: 20, position: "absolute", color: "gray",display: (typeof window !== 'undefined' && window.innerWidth <= 1024) ? 'none' : 'inline'}}>Powered by <a href="http://www.tracardi.com" style={{color: "black"}}>TRACARDI</a></span>
 }
